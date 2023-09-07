edfe3fee6cc0f24bc55a45c760016133424ae586
emirtanriverdi <139234223+emirtanriverdi@users.noreply.github.com>
2023-09-07 22:26:03 +0300

    Added files

---
+++ src/install/index.php
@@ -0,0 +1,85 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $host = $_POST['host'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $database = $_POST['database'];
+
+    // Bağlantı bilgilerini connect.php dosyasına kaydet
+    $connect_file = fopen('../connect.php', 'w');
+    if ($connect_file) {
+        $content = <<<EOD
+<?php
+\$host = '$host';
+\$username = '$username';
+\$password = '$password';
+\$database = '$database';
+\$conn = new mysqli(\$host, \$username, \$password, \$database);
+
+if (\$conn->connect_error) {
+    die("Bağlantı hatası: " . \$conn->connect_error);
+}
+?>
+EOD;
+        if (fwrite($connect_file, $content) === false) {
+            die("connect.php dosyasına yazılamadı.");
+        }
+        fclose($connect_file);
+
+        // SQL tablolarını oluştur
+        include 'sql.php';
+
+        // Admin oluşturma sayfasına yönlendir
+        header('Location: admin.php');
+        exit();
+    } else {
+        die("connect.php dosyası açılamadı veya yazılamadı.");
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Başla</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="../">Ana Sayfa</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="../register">Kayıt ol</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../login">Giriş yap</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../create">Konu oluştur</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container mt-5">
+    <h1>MySQL Bağlantı Bilgileri</h1>
+    <form method="POST">
+        <label for="host">Host:</label>
+        <input type="text" name="host" required><br><br>
+        <label for="username">Kullanıcı Adı:</label>
+        <input type="text" name="username" required><br><br>
+        <label for="password">Şifre:</label>
+        <input type="password" name="password" required><br><br>
+        <label for="database">Veritabanı Adı:</label>
+        <input type="text" name="database" required><br><br>
+        <input type="submit" value="Devam">
+    </form>
+    </div>
+</body>
+</html>
---
+++ src/install/sql.php
@@ -0,0 +1,47 @@
+<?php
+include '../connect.php';
+
+// Tabloları oluştur
+$sql = <<<EOD
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `message` text NOT NULL,
+  `message_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `user_id` int(11) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `topic_id` (`topic_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=48;
+
+CREATE TABLE IF NOT EXISTS `topics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `topic_title` varchar(255) NOT NULL,
+  `topic_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=8;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=2;
+EOD;
+
+if ($conn->multi_query($sql) === TRUE) {
+    echo "Tablolar başarıyla oluşturuldu.";
+} else {
+    echo "Tabloları oluştururken hata oluştu: " . $conn->error;
+}
+
+$conn->close();
+?>
