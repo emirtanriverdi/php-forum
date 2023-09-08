6ef87dbcb7104ef894b66afd8ccdf3b757da98e1
emirtanriverdi <139234223+emirtanriverdi@users.noreply.github.com>
2023-09-08 04:35:07 +0300

    Added topic list

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 // connect.php dosyasını kontrol et
 if (filesize('connect.php') === 0) {
     // connect.php dosyası boşsa install.php sayfasına yönlendir
-    header('Location: install');
+    header('Location: install.php');
     exit;
 }
 
@@ -30,8 +30,33 @@ if ($user_id !== null) {
     }
 }
 
+// Konu Başlıklarını Al
+$query = "SELECT * FROM topics ORDER BY topic_date DESC LIMIT 20";
+$result = mysqli_query($conn, $query);
 
-mysqli_close($conn);
+function formatElapsedTime($timestamp) {
+    $now = time();
+    $elapsed = $now - $timestamp;
+
+    if ($elapsed < 60) {
+        return $elapsed . ' saniye önce';
+    } elseif ($elapsed < 3600) {
+        $minutes = floor($elapsed / 60);
+        return $minutes . ' dakika önce';
+    } elseif ($elapsed < 86400) {
+        $hours = floor($elapsed / 3600);
+        return $hours . ' saat önce';
+    } elseif ($elapsed < 2592000) {
+        $days = floor($elapsed / 86400);
+        return $days . ' gün önce';
+    } elseif ($elapsed < 31536000) {
+        $months = floor($elapsed / 2592000);
+        return $months . ' ay önce';
+    } else {
+        $years = floor($elapsed / 31536000);
+        return $years . ' yıl önce';
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -81,9 +106,25 @@ mysqli_close($conn);
                 <div class="card-body">
                     <?php
                     if ($username !== "") {
-                        echo '<a href="logout" class="btn btn-danger">Çıkış Yap</a>';
+                        echo '<a href="logout" class="btn btn-danger">Çıkış Yap</a><br><br>';
                     } else {
                         echo '<p>Kayıt olmadan sınırlı erişime sahipsiniz. Lütfen kayıt olun veya giriş yapın.</p>';
+                    }
+                    ?>
+
+                    <!-- Konu Başlıkları -->
+                    <?php
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        // Tarih bilgisini hesaplayın (örneğin, $row['topic_date'] tarih olmalıdır).
+                        $topicDate = strtotime($row['topic_date']);
+                        $formattedDate = formatElapsedTime($topicDate);
+
+                        echo '<div class="card mb-3">';
+                        echo '<div class="card-body">';
+                        echo '<h5 class="card-title"><a href="topic?id=' . $row['id'] . '">' . htmlspecialchars($row['topic_title']) . '</a></h5>';
+                        echo '<p class="card-text">Konu oluşturuldu: ' . $formattedDate . '</p>';
+                        echo '</div>';
+                        echo '</div>';
                     }
                     ?>
                 </div>
@@ -95,5 +136,6 @@ mysqli_close($conn);
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<br>
 </body>
 </html>
